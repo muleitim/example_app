187a78088ecbd8dc1a70a549abce8abd7c69365f
Timothy Mulei <muleitimothy@gmail.com>
2025-06-23 18:46:23 +0300

    Implemented viewing individual user posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -14,4 +16,10 @@ class Post extends Model
         'title',
         'body'        
     ];
+
+    public function user() : BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
---
+++ resources/views/components/postCard.blade.php
@@ -0,0 +1,19 @@
+@props(['post'])
+
+<div class="card ">
+
+    {{-- Title --}}
+    <h2 class="font-bold text-xl"> {{ $post->title }} </h2>
+
+    {{-- Author and Date --}}
+    <div class="text-xs font-light mb-4">
+        <span>Posted {{ $post->created_at->diffForHumans() }} by </span>
+        <a href="{{ route('posts.user', $post->user) }}" class="text-blue-500 font-medium" >{{ $post->user->username }}</a>
+    </div>
+
+    {{-- Body --}}
+    <div class="text-sm">
+        <p>{{ Str::words($post->body, 15) }}</p>
+    </div>
+
+</div>
